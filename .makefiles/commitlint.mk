a0adc3eb5c4f9124b07dcc672da6c01aa0f9a939
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-05-06 12:01:29 -0500

    svc(setup): add rich commitlint reporting
    updates the commitlint package which includes a issues/checkstyl json
    report that can be read by machines and contains rich markdown reports
    for github. additionally updates the jenkins setup to push the report
    information so it is displayed in github checks and accurately reports
    the error
    
    Ref: REL-1791

--- .makefiles/commitlint.mk
+++ .makefiles/commitlint.mk
@@ -2,5 +2,5 @@ lint:: lint-commits
 
 
 .PHONY:lint-commits
-lint-commits: $(DOCKER_ENV) ## lint commits on current branch ahead of main
-	$(RUN_NO_ENV) npm run commitlint
+lint-commits: $(REPORT_DIR) $(DOCKER_ENV) ## lint commits on current branch ahead of main
+	$(RUN_NO_ENV) npm run commitlint $(if $(IS_CI),-- --format=checkstyle --report-dir=$(REPORT_DIR),)
